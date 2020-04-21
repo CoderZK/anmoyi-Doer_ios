2811e5cdc6b84cca9dd620b3a69a045d10fde2d7
kunzhang <542283618@qq.com>
2020-04-21 10:53:24 +0800

    rr

--- Pride/CLock/controller/AddClockView.h
+++ Pride/CLock/controller/AddClockView.h
@@ -11,6 +11,9 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface AddClockView : UIView
+- (void)show;
+- (void)diss;
+
 
 @end
 
--- Pride/CLock/controller/AddClockView.m
+++ Pride/CLock/controller/AddClockView.m
@@ -8,9 +8,37 @@
 
 #import "AddClockView.h"
 
+@interface AddClockView()
+@property(nonatomic , strong)UIView *blackV;
+@property(nonatomic , strong)UIDatePicker *datePicker;
+@property(nonatomic , strong)UILabel *timeLB;
+@property(nonatomic , strong)UISwitch *switchBt;
+
+@end
+
+
 @implementation AddClockView
 
-
+- (instancetype)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        self.backgroundColor =[UIColor colorWithWhite:0 alpha:0.1];
+        
+    
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    return  self ;
+}
 
 
 @end
